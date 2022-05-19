188e96a773911f3b38de0e1610023b0421ade538
Diego Bonilla <drbv27@gmail.com>
2022-05-19 15:17:59 -0500

    adding some styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Testimonio from './components/Testimonio';
 function App() {
   return (
     <div className="App">
+      <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp :</h1>
       <Testimonio/>
+      </div>
     </div>
   );
 }
